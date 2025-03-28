9ca26544afc7b5da754648331fd57a68184fbca2
ILIYA LESANI <iliyalesani@gmail.com>
2025-03-29 01:31:56 +0330

    separate dev and prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: '/azeroth-arena/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  
+  return {
+    base: isProduction ? '/azeroth-arena/' : '/',
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name].js',
+          chunkFileNames: 'assets/[name].js',
+          assetFileNames: 'assets/[name].[ext]'
+        }
       }
+    },
+    server: {
+      port: 3000
     }
-  },
-  server: {
-    port: 3000
-  }
+  };
 }); 
